070da85d69cdc837ba8e1581e59e9875a86ff4c8
Pack <pans4826@gmail.com>
2024-06-10 16:01:42 +0700

    first commit

---
+++ lab1.2.py
@@ -0,0 +1,7 @@
+b = int(input("รับค่าฐาน: "))
+h = int(input("รับค่าสูง: "))
+#print("ค่าฐานคือ ", b)
+#print("ค่าสูงคือ ", h)
+area = 1/2 * b * h
+print("พื้นที่ =", str(area))
+
---
+++ lab1.3.py
@@ -0,0 +1,4 @@
+r = int(input("Radius :"))
+area = 22/7*(r*r)
+print ("พื้นที่วงกลม :", str(area))
+        
