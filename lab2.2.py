9a09ca958af723e76fd920b14f90c99a4454e00a
Pack <pans4826@gmail.com>
2024-06-17 14:52:14 +0700

    update

--- lab2.2.py
+++ lab2.2.py
@@ -1,17 +1,20 @@
-score =input("ใส่คะแนน : ")
+score = input("ใส่คะแนน : ")
+
 if score.isnumeric():
-        if 80 <= score <=100:
+    score = int(score)
+    # แก้ไขเงื่อนไขที่ผิดพลาด
+    if 0 <= score <= 100:
+        if 80 <= score <= 100:
             print("เกรด A")
         elif 70 <= score <= 79:
             print("เกรด B")
         elif 60 <= score <= 69:
             print("เกรด C")
         elif 50 <= score <= 59:
-                print("เกรด D")if 0 <= score <= 100:
+            print("เกรด D")
         elif 0 <= score <= 49:
-                print("เกรด F")
-        else:
-            print("กรุณากรอกข้อมูล 0-100")
+            print("เกรด F")
+    else:
+        print("กรุณากรอกข้อมูล 0-100")
 else:
-    print("กรุณากรอกข้อมูล 1-100")
-    
+    print("กรุณากรอกข้อมูล 0-100")
