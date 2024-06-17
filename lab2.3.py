b3776a3742e895bf613fd2202112f7f88b026ea6
Pack <pans4826@gmail.com>
2024-06-17 14:56:41 +0700

    update

--- lab2.3.py
+++ lab2.3.py
@@ -7,14 +7,12 @@ height = float(height) / 100
 if weight <= 0 or height <= 0:
     print("กรุณากรอกน้ำหนักและส่วนสูงที่มากกว่า 0")
 else:
-    # คำนวณ BMI
+
     bmi = weight / (height ** 2)
     bmi = round(bmi, 2)
 
-    # แสดงผลลัพธ์
     print(f"BMI ของคุณคือ: {bmi}")
 
-    # กำหนดเกณฑ์ของ BMI
     if bmi < 18.50:
         print("อยู่ในเกณฑ์: น้ำหนักน้อย / ผอม (มากกว่าค่ามาตรฐาน)")
     elif 18.50 <= bmi <= 22.90:
