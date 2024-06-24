2c07a9bb78f5bca883a17321e7a8313c541f1ec0
Pack <pans4826@gmail.com>
2024-06-24 15:15:48 +0700

    update

---
+++ lab4.2.py
@@ -0,0 +1,7 @@
+#Tuple.type Immutable
+person = (35,52,18.23,195,155)
+#for p in person:
+#    print(p)
+print(person[-4:-3])
+#person[0] = 100
+#print(person[0:])
---
+++ lab4.3.py
@@ -0,0 +1,9 @@
+person = {"name":"Kanathip","age":35,"wt":75,"h":172}
+#for p in person:
+#    print(person[p])
+print(person["name"])
+print(person["age"])
+print(person["wt"])
+print(person["h"])
+person["name"] = "Pack"
+print(person["name"])
